13f0845b81813038018a64a2b4f72320681850e5
saman <ssaman.parkaleh76@gmail.com>
2023-05-18 22:53:45 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const fetchimgs = () => {
+    fetch(
+      `https://api.unsplash.com/search/photos/?client_id=33S-w7ZyQKIKrQ7DPPKP2UrbLyC40hUaz4oriIrh_K4&query=${value}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setResult(data.results);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contaner ">
+      <div
+        className="heder bg-info d-flex align-items-center justify-content-center "
+        style={{ height: "70px" }}
+      >
+        <input
+          className="m-end-5 p-2 rounded"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          style={{ width: "40%" }}
+          placeholder={"Search. . . (:"}
+        />
+        <button onClick={fetchimgs} className="m-5 p-2 border rounded  ">
+          send
+        </button>
+      </div>
+      <div className="box ">
+        {result &&
+          result.map((item) => (
+            <img
+              className=" img p-5   border border-1  m-2 bg-light"
+              key={item.id}
+              src={item.urls.regular}
+              alt=""
+            />
+          ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './Conponent/Bootstraps/bootstrap.min.css'
+import './Conponent/Bootstraps/bootstrap.rtl.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
